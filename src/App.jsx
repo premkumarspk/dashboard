a61689ec71d26a38569df4fa95fa6ab98c9fde30
premkumar-spk <premkumarspk4321@gmail.com>
2024-02-17 12:40:48 +0530

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Signup from './pages/Signup'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+      <Signup/>
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Logo from '../css/klipartz.png'
+import '../css/signup.css'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Register from './Register';
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub,FaDiscord,FaLinkedin,FaTwitter,FaApple} from "react-icons/fa";
+function signup() {
+  return (
+    <div className='contain  '> 
+      <div className="companyinfo">
+      <div className="icon">
+      <img src={Logo} alt="logo" width={100} height={100} />
+      </div>
+        <h1 class="company-info-logo "> base</h1>
+        <div className="icon-div">
+          <FaGithub className='social-icons'/>
+          <FaDiscord className='social-icons'/>
+          <FaLinkedin className='social-icons'/>
+          <FaTwitter className='social-icons'/>
+        </div>
+      </div>
+      <div className="signup">
+        <h3>Sign in</h3>
+        <h4> sign in your account</h4>
+        <div className="bttn">
+          <button  id ="google"  className='sign-logo'> <FcGoogle/> <span> signin in with Google </span> </button>
+          <button className='sign-logo'> <FaApple/> <span> signin in with Apple </span> </button>
+        </div>
+        <div className="email">
+          <label htmlFor="">Email address</label><br/>
+          <input type='text' placeholder='johndoe@gmail.com'/> <br/>
+          <label htmlFor="">password</label> <br />
+          <input type="password"  placeholder='pasword@123'/> <br/>
+          <a href='#'> Forget password?</a>
+          <div className='sign-btn'> 
+          <button> Sign In</button>
+          </div>
+        </div>
+
+        <p> Don't have an account?  <a > Register herer </a></p>
+      </div>
+    </div>
+  )
+}
+
+export default signup
